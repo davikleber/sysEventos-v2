80691898577a3ba90bee9bb4f84ebb21c5fa5398
davikleber <davikleber@outlook.com>
2022-10-18 12:20:34 -0300

    Atualização Estrutural sysEventos

---
+++ Back/src/sysEventos.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sysEventos.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Back/src/sysEventos.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sysEventos.Persistence
+{
+    public class Class1
+    {
+    }
+}
